a41a354bfa1151ce660d9d56491c8c1a48d89381
Dmitry <Dololob92@gmail.com>
2024-10-16 14:54:36 +0300

    test complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+
+  let openBr = bracketsConfig.map(arrOfPair => arrOfPair[0]); 
+  let closeBr = bracketsConfig.map(arrOfPair => arrOfPair[1]);
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (openBr.includes(char)) {
+      if (closeBr.includes(char) && stack[stack.length - 1] === char) {
+        stack.pop(char);
+      } else {
+        stack.push(char);
+      }
+    } else { 
+      let pairOpenBr = openBr[closeBr.indexOf(char)];
+      if (stack.length === 0 || pairOpenBr !== stack.pop()) return false;
+    }
+  }
+
+  return stack.length === 0;
 }
